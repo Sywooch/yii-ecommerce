0f696223d3a5df0d37ea456f4ad75cf20447c597
shockedbear <shockedbear@gmail.com>
2016-07-27 16:11:19 +0700

    Added migration

---
+++ common/migrations/m160726_130845_create_countries_with_taxes.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+class m160726_130845_create_countries_with_taxes extends Migration
+{
+    public function up()
+    {
+        $this->createTable('countries', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'abbr' => $this->string()->notNull()->unique(),
+            'exists_tax' => $this->integer()->defaultValue(0),
+            'tax' => $this->float(),
+        ]);
+
+        $countries = json_decode(file_get_contents('http://country.io/names.json'), true);
+
+        foreach ($countries as $abbr => $name) {
+            $this->insert('countries', [
+                'name' => $name,
+                'abbr' => $abbr,
+            ]);
+        }
+
+        $this->addColumn('orders', 'country', 'VARCHAR(255) NOT NULL');
+        $this->addColumn('orders', 'tax', 'FLOAT DEFAULT NULL');
+    }
+
+    public function down()
+    {
+        $this->dropColumn('orders', 'country');
+        $this->dropColumn('orders', 'tax');
+
+        $this->dropTable('countries');
+    }
+}
